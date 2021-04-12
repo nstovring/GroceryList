96b7be1814fb07e7a0f1108ad915dc996f228e3b
Nikolaj Støvring <nstovring@live.dk>
2021-04-12 15:10:49 +0200

    Initialize

---
+++ Assets/ShoppingListManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShoppingListManager : MonoBehaviour
+{
+    public GameObject textField;
+
+    public GameObject textfieldParent;
+
+
+    //public List<RectTransform> items;
+
+    public int textOffset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //items = new List<RectTransform>();
+    }
+
+
+    public void TextReset(InputField f)
+    {
+        f.text = "";
+    }
+
+
+    public void DeleteButton(GameObject g)
+    {
+        Destroy(g);
+    }
+
+
+    public void Test(string input)
+    {
+        Debug.Log(input);
+
+        GameObject newTextField = Instantiate(textField, Vector3.zero, Quaternion.identity,textfieldParent.transform);
+
+        Text newTextFieldText = newTextField.GetComponent<Text>();
+        newTextFieldText.text = input;
+
+        RectTransform rect = newTextField.GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(0, textOffset);
+        textOffset -= 20;
+        
+        
+
+
+        //items.Add(rect);
+
+        //UpdateItems();
+    }
+
+    //public void RemoveItem(RectTransform item)
+    //{
+    //    items.Remove(item);
+    //    UpdateItems();
+    //    Destroy(item.gameObject);
+    //}
+    //
+    //public void UpdateItems()
+    //{
+    //    textOffset = 0;
+    //    for (int i = 0; i < items.Count; i++)
+    //    {
+    //        items[i].anchoredPosition = new Vector2(0, textOffset);
+    //        textOffset -= 20;
+    //    }
+    //}
+}
